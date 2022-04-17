8db402db5f1b447a45a242f26468595eb3150c40
leonsharvakov <leons554477@gmail.com>
2022-04-17 12:07:20 +0300

    lel

--- only/only.js
+++ only/only.js
@@ -1,22 +1,32 @@
-const BORDEER_SIZE=8
+const BORDEER_SIZE=8;
+
 
 function chess(){
   const table= document.createElement('table');
-document.body.append(table);
+  table.classList.add('border')
+document.body.appendChild(table);
 for(let i=0;i<BORDEER_SIZE; i++)
 {
-const row=table.insertRow();
+    const row=table.insertRow();
   for(let j=0;j<BORDEER_SIZE; j++){
     const cell= row.insertCell()
+    
+
+
+    
     if((i+j) % 2 ==0){
       cell.className='w'}
      else
        {
-        cell.className='b'}
+        cell.className='b'
+  
+      }
       
       }
     }
+    
   }
+  window.addEventListener('load',chess)
 
 
 
